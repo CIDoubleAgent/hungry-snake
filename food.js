d3b9520046b8f767fa9c9ddfc8e89ac3afc682ac
Nick Zabkar <nzabkar51@gmail.com>
2022-08-30 19:32:33 -0400

    randomized food renders

--- food.js
+++ food.js
@@ -1,7 +1,14 @@
-let food = { x: 1, y: 1 };
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
+
+let food = getRandomFoodPosition();
+const EXPANSION_RATE = 5;
 
 export function update() {
-
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE);
+        food = getRandomFoodPosition();
+    }
 }
 
 export function render(gameBoard) {
@@ -10,5 +17,12 @@ export function render(gameBoard) {
     foodElememt.style.gridColumnStart = food.x;
     foodElememt.classList.add('food')
     gameBoard.appendChild(foodElememt)
-    
+}
+
+function getRandomFoodPosition() {
+    let newFoodPosition;
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
 }
--- snake.js
+++ snake.js
@@ -2,8 +2,10 @@ import { getInputDirection } from "./input.js";
 
 export const snakeSpeed = 5;
 const snakeBody = [{ x: 11, y: 11 }]
+let newSegments = 0;
 
 export function update() {
+    addSegments()
     const inputDirection = getInputDirection();
     for (let i = snakeBody.length -2; i >= 0; i--) {
         snakeBody[i + 1] = { ...snakeBody[i] }
@@ -21,4 +23,26 @@ export function render(gameBoard) {
         snakeElememt.classList.add('snake')
         gameBoard.appendChild(snakeElememt)
     })
+}
+
+export function expandSnake(amount) {
+    newSegments += amount;
+}
+
+export function onSnake(position) {
+    return snakeBody.some(segment => {
+        return equalPositions(segment, position)
+    })
+}
+
+function equalPositions(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y
+}
+
+function addSegments() {
+    for (let i = 0; i < newSegments; i++) {
+        snakeBody.push({ ...snakeBody[snakeBody.length -1] })
+    }
+
+    newSegments = 0;
 }
