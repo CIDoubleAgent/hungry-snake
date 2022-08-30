8836ad46ec09e5e5beee1b497afc49be69d51ec9
Nick Zabkar <nzabkar51@gmail.com>
2022-08-30 14:33:57 -0400

    Added input.js file to control snake movement

--- snake.js
+++ snake.js
@@ -1,19 +1,16 @@
+import { getInputDirection } from "./input.js";
+
 export const snakeSpeed = 1;
-const snakeBody = [
-    { x: 10, y: 11 },
-    { x: 11, y: 11 }, 
-    { x: 12, y: 11 }, 
-    { x: 13, y: 11 }, 
-    { x: 14, y: 11 }
-]
+const snakeBody = [{ x: 11, y: 11 }]
 
 export function update() {
+    const inputDirection = getInputDirection();
     for (let i = snakeBody.length -2; i >= 0; i--) {
         snakeBody[i + 1] = { ...snakeBody[i] }
     }
 
-    // snakeBody[0].x += 0
-    // snakeBody[0].y += 1
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
 }
 
 export function render(gameBoard) {
