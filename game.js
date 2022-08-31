8a5137bea592450eb517a6e3ea8a43092badebf7
Nick Zabkar <nzabkar51@gmail.com>
2022-08-30 20:02:00 -0400

    Added failure states for game

--- game.js
+++ game.js
@@ -1,9 +1,19 @@
-import { update as updateSnake, render as renderSnake, snakeSpeed } from './snake.js'
-import { update as updateFood, render as renderFood } from "./food.js"
+import { update as updateSnake, render as renderSnake, snakeSpeed, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, render as renderFood } from './food.js'
+import { outsideGrid } from './grid.js'
+
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
+    if (gameOver) {
+        if (confirm('Game Over, press OK to restart.')) {
+            window.location = '/'
+        }
+        return;
+    }
+
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
     if(secondsSinceLastRender < 1 / snakeSpeed) return;
@@ -19,10 +29,15 @@ window.requestAnimationFrame(main);
 function update() {
     updateSnake();
     updateFood();
+    checkDeath();
 }
 
 function render() {
     gameBoard.innerHTML = ''
     renderSnake(gameBoard);
     renderFood(gameBoard);
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
