18a39ad2e17c93548744e0d3d80f3808296a8076
Nick Zabkar <nzabkar51@gmail.com>
2022-09-23 18:28:07 -0400

    Added controls for mobile

--- input.js
+++ input.js
@@ -22,6 +22,27 @@ window.addEventListener('keydown', e => {
     }
 });
 
+window.addEventListener('pointerdown', e => {
+    switch (e.target.id) {
+        case 'up':
+            if (lastInputDirection.y !==0 ) break
+            inputDirection = { x: 0, y: -1 };
+            break
+        case 'down':
+            if (lastInputDirection.y !==0 ) break
+            inputDirection = { x: 0, y: 1 };
+            break
+        case 'left':
+            if (lastInputDirection.x !==0 ) break
+            inputDirection = { x: -1, y: 0 };
+            break
+        case 'right':
+            if (lastInputDirection.x !==0 ) break
+            inputDirection = { x: 1, y: 0 };
+            break
+    }
+});
+
 export function getInputDirection() {
     lastInputDirection = inputDirection
     return inputDirection;
